3fd5a6e018124dd2db4e5c0d45005379d3c6b211
Barry <whitekis@163.com>
2020-07-09 17:24:21 +0800

    add new fun

---
+++ DiguiSuanfa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiguiSuanfa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program p = new Program();
+            while (true)
+            {
+                int n = 0;
+                int.TryParse(Console.ReadLine(),out n);
+                Console.WriteLine(Recursion.Fun(n).ToString());
+            }
+        }
+
+        public long Rerision(long n)
+        {
+            if (n == 0)
+                return 1;
+            else
+            return Rerision(n - 1) * n;
+
+        }
+    }
+}
---
+++ DiguiSuanfa/Recursion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiguiSuanfa
+{
+    public class Recursion
+    {
+        public static int Fun(int n)
+        {
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 2;
+            return Fun(n - 2) + Fun(n - 1);
+        }
+    }
+}
